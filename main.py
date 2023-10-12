a4fedc60bedd001a2aeb6a35b9b9eda529332cc0
fullcircle <kevin@fullcirclenetworks.com>
2023-10-12 14:32:52 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,41 @@
+import simpy
+
+# Define a simple network node
+class NetworkNode:
+    def __init__(self, env, name):
+        self.env = env
+        self.name = name
+        self.queue = simpy.Store(env)
+
+    def send(self, data, dest):
+        yield self.queue.put(data)
+        print(f'{self.name} sent: {data}')
+        yield self.env.timeout(1)  # Simulate transmission time
+        yield self.env.process(dest.receive(data))  # Call the receive function of the destination node
+
+    def receive(self, data):
+        print(f'{self.name} received: {data}')
+        yield self.env.timeout(1)  # Simulate processing time
+
+# Define a simulation process
+def data_transfer(env, node1, node2, duration):
+    data = "Hello, iWARP!"
+    start_time = env.now
+    while env.now - start_time < duration:
+        yield env.process(node1.send(data, node2))
+
+# Create the simulation environment
+env = simpy.Environment()
+
+# Create network nodes
+node_A = NetworkNode(env, "Node A")
+node_B = NetworkNode(env, "Node B")
+
+# Get user-defined simulation duration
+simulation_duration = float(input("Enter simulation duration (in seconds): "))
+
+# Start the data transfer process
+env.process(data_transfer(env, node_A, node_B, simulation_duration))
+
+# Run the simulation
+env.run(until=simulation_duration)
